7c73bb47858a123bd68719ea3377d2d992605e45
Shayna <shaynak112@gmail.com>
2017-06-12 03:10:33 -0400

    Add files via upload

---
+++ adminNav.php
@@ -0,0 +1,142 @@
+ <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <a href='index.php'><p>Lexy Esposito</p>
+          <p>Admin</p></a>
+        </div>
+      </div>
+
+ <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">Navigation</li>
+        
+        
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-music"></i>
+            <span>Events</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="eventsAdd.php"><i class="fa fa-circle-o"></i>Add Event</a></li>
+            <li><a href="eventsUpdate.php"><i class="fa fa-circle-o"></i>Update Event</a></li>
+            <li><a href="eventsAll.php"><i class="fa fa-circle-o"></i>All Events</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="pages/guestlists.php">
+            <i class="fa fa-pencil"></i> <span>Guestlists</span>
+            <span class="pull-right-container">
+              <!--<small class="label pull-right bg-green">new</small>-->
+            </span>
+          </a>
+        </li>
+        
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-camera"></i>
+            <span>Gallery</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/gallery/addPhotos.php"><i class="fa fa-circle-o"></i>Add Photos</a></li>
+            <li><a href="pages/gallery/approvePhotos.php"><i class="fa fa-circle-o"></i>Approve Photos</a></li>
+            <li><a href="pages/gallery/updatePhotos.php"><i class="fa fa-circle-o"></i>Update/Delete Photos</a></li>
+            <li><a href="pages/gallery/comments.php"><i class="fa fa-circle-o"></i>Review Comments</a></li>
+            <li><a href="pages/gallery/links.php"><i class="fa fa-circle-o"></i>Update Gallery Links</a></li>
+          </ul>
+        </li>
+
+         <li>
+          <a href="venues.php">
+            <i class="fa fa-plane"></i> <span>Venues</span>
+            <span class="pull-right-container">
+              <!--<small class="label pull-right bg-green">new</small>-->
+            </span>
+          </a>
+        </li>
+
+
+         <li>
+          <a href="partners.php">
+            <i class="fa fa-users"></i> <span>Partners</span>
+            <span class="pull-right-container">
+              <!--<small class="label pull-right bg-green">new</small>-->
+            </span>
+          </a>
+        </li>
+
+
+         <li>
+          <a href="otherEvents.php">
+            <i class="fa fa-ticket"></i> <span>Other Events</span>
+            <span class="pull-right-container">
+              <!--<small class="label pull-right bg-green">new</small>-->
+            </span>
+          </a>
+        </li>
+
+        <li>
+          <a href="subscribers.php">
+            <i class="fa fa-road"></i> <span>Subscribers</span>
+            <span class="pull-right-container">
+              <!--<small class="label pull-right bg-green">new</small>-->
+            </span>
+          </a>
+        </li>
+
+         <li>
+          <a href="approveUser.php">
+            <i class="fa fa-user"></i> <span>Approve User</span>
+            <span class="pull-right-container">
+              <!--<small class="label pull-right bg-green">new</small>-->
+            </span>
+          </a>
+        </li>
+
+         <li>
+          <a href="adminIdeas.php">
+            <i class="fa fa-user"></i> <span>Admin Ideas</span>
+            <span class="pull-right-container">
+              <!--<small class="label pull-right bg-green">new</small>-->
+            </span>
+          </a>
+        </li>
+
+
+        <li>
+          <a href="pages/calendar.html">
+            <i class="fa fa-calendar"></i> <span>Calendar</span>
+            <span class="pull-right-container">
+              <!--<small class="label pull-right bg-red">3</small>
+              <small class="label pull-right bg-blue">17</small>-->
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="pages/mailbox/mailbox.html">
+            <i class="fa fa-envelope"></i> <span>Mailbox</span>
+            <span class="pull-right-container">
+              <!--<small class="label pull-right bg-yellow">12</small>
+              <small class="label pull-right bg-green">16</small>
+              <small class="label pull-right bg-red">5</small>-->
+            </span>
+          </a>
+        </li>
+        
+
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
